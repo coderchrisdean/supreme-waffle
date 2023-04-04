4481435975a914f7f59b5b912b27c394abf06462
coderchrisdean <coderchrisdean@gmail.com>
2023-04-04 10:38:37 -0700

    added Navigation js and styling, also set up assets/image directory for avatar

---
+++ christopher-dean-port/src/components/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './Navigation.css';
+
+const Navigation = () => {
+    const [activeLink, setActiveLink] = useState('About Me');
+    const handleNavClick = (link) => {
+        setActiveLink(link);
+};
+
+return (
+    <nav className="navigation">
+        <ul className="nav-list">
+        {['About Me', 'Portfolio', 'Contact', 'Resume'].map((link) => (
+            <li key={link} className={`nav-item${activeLink === link ? 'active' : ''}`}>
+                <button onClick={() => handleNavClick(link)} className="nav-link">{link}</button>
+            </li>
+        ))}
+        </ul>
+        </nav>
+);
+        };
+
+        export default Navigation;
