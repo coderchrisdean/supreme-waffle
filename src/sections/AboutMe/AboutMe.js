b02aeed7f0974f7960ecf11e48db7060b9c7d932
coderchrisdean <coderchrisdean@gmail.com>
2023-04-06 15:41:49 -0700

    updated Bio

--- src/sections/AboutMe/AboutMe.js
+++ src/sections/AboutMe/AboutMe.js
@@ -1,16 +1,35 @@
 import React from "react";
 import "./AboutMe.css";
 
-
 const AboutMe = () => {
   return (
     <div className="about-me">
-     
       <div className="about-me-text">
         <h2>About Me</h2>
         <p>
-          Bio about Christopher Dean, temporary info 123. ABC. Will fill this
-          out later. UC Berkeley Coding Bootcamp student I am amazing.
+          I'm a Full Stack Web Developer with a passion for designing and
+          developing websites and applications that are both aesthetically
+          pleasing and functional. I love the challenge of problem-solving and
+          building dynamic solutions that improve user experience.
+        </p>
+        <p>
+          As a recent graduate from the UC Berkeley Full Stack Web Development
+          program, I have had the opportunity to work on a wide range of
+          projects, from e-commerce websites to content management systems. My
+          expertise lies in front-end development, including HTML, CSS, and
+          JavaScript. I am also proficient in back-end development and have
+          experience working with databases, server-side scripting, and APIs.
+        </p>
+        <p>
+          Aside from coding, I enjoy staying up to date with the latest web
+          development trends and technologies, as well as exploring new tools
+          and frameworks. When I'm not working on a project, you can find me
+          hiking, keeping up with friends and family, and staying up to date
+          with news and current events.
+        </p>
+        <p>
+          If you're interested in working together, feel free to reach out! I'm
+          always excited to collaborate and bring new ideas to life.
         </p>
       </div>
     </div>
